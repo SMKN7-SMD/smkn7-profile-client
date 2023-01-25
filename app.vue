9a43e4d76f25ffe5e7c98c7b696185060a1fbbf0
Bagus Nur A <bagusnur@protonmail.com>
2023-01-25 18:25:45 +0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: 'Montserrat', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+  ],
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    },
+    preconnect: true,
+    preload: true,
+  }
+})
